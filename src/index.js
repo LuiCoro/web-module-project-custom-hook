7a256279966ec3834b26ba647667ce3b69e300b2
LuiCoro <luiacorona029@icloud.com>
2021-09-07 20:57:31 -0700

    'Added' - Created the useForm Hook for the MVP - props are passed correctly 'Removed' - axios from index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
+import useForm from './Hooks/useForm'
 
 import "./styles.scss";
 
-const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
+
+const App = () => {
+  
+  const [coinData, darkMode, setDarkMode] = useForm()
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
